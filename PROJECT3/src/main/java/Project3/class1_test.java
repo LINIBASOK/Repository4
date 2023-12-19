7fd1d272a18d3dde9d57004519a32861bf59309e
Lini B Asok <Lini B Asok@TYSS-Lini-B-Asok>
2023-12-19 11:00:43 +0530

    add

---
+++ PROJECT3/src/main/java/Project3/class1_test.java
@@ -0,0 +1,12 @@
+package Project3;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class class1_test {
+	@Test
+	public void m1() {
+		Reporter.log("started..");
+	}
+
+}
